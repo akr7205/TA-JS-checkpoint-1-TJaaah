d297ba05084e43e5c19e8a15930e224895a4d1b7
Arun <akr7205@gmail.com>
2022-04-03 00:55:12 +0530

    2nd commit

--- block-BJaafx/code/index.js
+++ block-BJaafx/code/index.js
@@ -1,21 +1,21 @@
 // 1. Using loops take 10 inputs from user and find the average of all the numbers.
-// let sum = 0;
-// for (let i = 0; i <= 10; i++) {
-//     let num = +prompt(`enter number ${i}`);
-//     sum += num;
-// }
-// let avg = sum / 10;
-// console.log(avg);
+let sum = 0;
+for (let i = 0; i <= 10; i++) {
+    let num = +prompt(`enter number ${i}`);
+    sum += num;
+}
+let avg = sum / 10;
+console.log(avg);
 
 // 2. What will be the output of the code below
 
-// ```js
-// let i = 0;
-// while (i < 3) {
-//   println('hi');
-//   i++;
-// }
-// ```
+```js
+let i = 0;
+while (i < 3) {
+  println('hi');
+  i++;
+}
+```
 // error because println is not defined in javascript
 
 // 3. Write a function named `getEvenSum` that accepts a parameter `max`. Return the sum of all even numbers. The value of max should default to 10.
@@ -62,52 +62,52 @@ console.log(getProductOfDigits(123));
 
 // 6. What will be the output of the following code below in multiple conditions? Explain with reason?
 
-// ```js
-// function check(num) {
-//   if (num > 5) {
-//     return 'Bigger than 5';
-//   }
+```js
+function check(num) {
+  if (num > 5) {
+    return 'Bigger than 5';
+  }
 
-//   if (num < 5) {
-//     return 'Smaller than 5';
-//   }
+  if (num < 5) {
+    return 'Smaller than 5';
+  }
 
-//   return num;
-// }
+  return num;
+}
 
-// check(10); // output   'Bigger than 5'
-// check(1); // output   'Smaller than 5'
-// check(5); // output    5
-// ``` 
+check(10); // output   'Bigger than 5'
+check(1); // output   'Smaller than 5'
+check(5); // output    5
+``` 
 
 // 7. What will be the output of the following code given below? Explain the reason?
 
-// ```js
-// function getOutput(name) {
-//   if (name === 'Arya') return 'You are arya';
-//   if (name === 'John') return 'You are john';
-//   return 'Who are you';
-// }
+```js
+function getOutput(name) {
+  if (name === 'Arya') return 'You are arya';
+  if (name === 'John') return 'You are john';
+  return 'Who are you';
+}
 
-// getOutput('Arya'); // what will be the output   'You are arya'
-// getOutput('John'); // what will be the output  'You are john'
-// getOutput(); // what will be the output   'Who are you'
-// ```
+getOutput('Arya'); // what will be the output   'You are arya'
+getOutput('John'); // what will be the output  'You are john'
+getOutput(); // what will be the output   'Who are you'
+```
 
 // 8. What will be the output of the following code given below? Explain the reason?
 
-// ```js
-// function getOutput(name) {
-//   if (name === 'Arya') console.log('You are arya');
-//   if (name === 'John') console.log('You are john');
-//   return 'Who are you';
-// }
+```js
+function getOutput(name) {
+  if (name === 'Arya') console.log('You are arya');
+  if (name === 'John') console.log('You are john');
+  return 'Who are you';
+}
 
-// getOutput('Arya'); // what will be the output 'You are arya'
-// getOutput('John'); // what will be the output  You are john
-// getOutput(); // what will be the output        'Who are you'
+getOutput('Arya'); // what will be the output 'You are arya'
+getOutput('John'); // what will be the output  You are john
+getOutput(); // what will be the output        'Who are you'
  
-// ```
+```
 
 // 9. Can a function have multiple return statement? Give one example if possible and explain the reason.
 
@@ -133,4 +133,4 @@ while (boolean condition)
 {
 loop statements...
 }
-// for is used when we know about intialization and incerement of value whereas while is use dwhen we decide to incement the value latter
+// for is used when we know about intialization and incerement of value whereas while is used when we decide to incement the value latter
